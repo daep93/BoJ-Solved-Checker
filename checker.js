57cabeb70ec28c98d3aeb4df63531a936e9d2675
daep93 <eoy1313@gmail.com>
2020-09-06 17:11:02 +0900

    success v1
    first version

--- checker.js
+++ checker.js
@@ -1,73 +1,69 @@
+// 확장프로그램 클릭시 init()이 작동함.
+init();
+function init() {
+  findProbNum(true, 0, "0");
+}
+
+// 현재 탭의 url을 받음.
+function findProbNum(mode, id, userID) {
+  chrome.tabs.query(
+    { active: true, lastFocusedWindow: true, currentWindow: true },
+    function (tabs) {
+      let url = tabs[0].url;
+      getProbNum(url, mode, id, userID);
+    }
+  );
+}
+
+// url 말미에 적힌 문제 번호를 받음.
+function getProbNum(url, mode, id, userID) {
+  let i;
+  for (i = url.length - 1; url[i] != "/"; i--) {}
+  const probNum = url.substring(i + 1, url.length);
+  document.querySelector(".problem_number").innerText = probNum;
+  loadName(probNum, mode, id, userID);
+}
+
+// key가 user인 value들을 받아서 for each문을 통해 주어진 문제를 풀었는지 확인.
+function loadName(probNum, mode, id, userID) {
+  if (mode) {
+    const currentUsers = localStorage.getItem("user");
+    if (currentUsers !== null) {
+      const parsedUsers = JSON.parse(currentUsers);
+      parsedUsers.forEach((element) => {
+        checkSolved(element.id, element.text, probNum);
+      });
+    }
+  } else {
+    checkSolved(id, userID, probNum);
+  }
+}
+
+// 해당 userID의 문제풀이 내역을 확인하고 성공했을 경우, id를 따라서 결과들을 저장함.
 function checkSolved(id, userID, probNum) {
   const userURL = "https://www.acmicpc.net/user/" + userID;
   console.log(userURL);
   const xhr = new XMLHttpRequest();
-  // 비동기 방식으로 Request를 오픈한다
+
   xhr.open("GET", userURL);
-  // Request를 전송한다
   xhr.send();
 
-  // Event Handler
   xhr.onreadystatechange = function () {
-    // 서버 응답 완료 && 정상 응답
     if (xhr.readyState !== XMLHttpRequest.DONE) {
-      console.log("readyState!==XMLHttpReq");
       return;
     }
 
     if (xhr.status === 200) {
-      // console.log("success: " + xhr.responseType);
       const numberText = xhr.responseText;
-      console.log(probNum);
-      console.log(typeof probNum);
 
-      // const myNum = numberText.match(new RegExp("\\b" + probNum + "\\b", "gi")).length;
       const myNum = numberText.search(new RegExp("\\b" + probNum + "\\b"));
-      console.log("myNum: " + myNum);
       if (myNum !== -1) {
         document.getElementById(id).querySelector("span").innerText = "solved";
       } else {
         document.getElementById(id).querySelector("span").innerText = "yet";
       }
     } else {
-      console.log("fail");
       console.log(`[${xhr.status}] : ${xhr.statusText}`);
     }
   };
 }
-
-function loadName(probNum) {
-  const currentUsers = localStorage.getItem("user");
-  console.log(currentUsers);
-  if (currentUsers !== null) {
-    const parsedUsers = JSON.parse(currentUsers);
-    parsedUsers.forEach((element) => {
-      console.log(element.text);
-      checkSolved(element.id, element.text, probNum);
-    });
-  }
-}
-
-function getProbNum(url) {
-  let i;
-  for (i = url.length - 1; url[i] != "/"; i--) {}
-  const probNum = url.substring(i + 1, url.length);
-  document.querySelector(".problem_number").innerText = probNum;
-  loadName(probNum);
-}
-
-function findProbNum() {
-  chrome.tabs.query(
-    { active: true, lastFocusedWindow: true, currentWindow: true },
-    function (tabs) {
-      let url = tabs[0].url;
-      getProbNum(url);
-    }
-  );
-}
-
-function init() {
-  findProbNum();
-}
-
-init();
--- userMgr.js
+++ userMgr.js
@@ -5,21 +5,34 @@ const userList = document.querySelector(".BoJ-user-list"),
 const USER_LS = "user";
 let users = [];
 
-function saveUsers() {
-  localStorage.setItem(USER_LS, JSON.stringify(users));
+// 확장프로그램 실행시 checker보다 먼저 올라간다.
+init();
+function init() {
+  loadName();
+  userForm.addEventListener("submit", handleSubmit);
 }
 
-function deleteUser(event) {
-  const btn = event.target;
-  const li = btn.parentNode;
-  userList.removeChild(li);
-  const cleanUsers = users.filter((element) => {
-    return element.id !== Number(li.id);
-  });
-  toDos = cleanUsers;
-  saveUsers();
+// localStorage에 저장해놓은 이름들을 불러서 showUser함수를 통해 display한다.
+function loadName() {
+  const currentUsers = localStorage.getItem(USER_LS);
+  if (currentUsers !== null) {
+    const parsedUsers = JSON.parse(currentUsers);
+    parsedUsers.forEach((element) => {
+      showUser(element.text);
+    });
+  }
 }
 
+// userID를 입력하고 submit을 한 경우, showUser로 넘어간다.
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = userInput.value;
+  showUser(currentValue);
+  userInput.value = "";
+  findProbNum(false, users.length, users[users.length - 1].text);
+}
+
+// showUser를 호출시에는 userObj를 만들어서 localStorage에 저장된 users에 새롭게 user를 추가한다.
 function showUser(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
@@ -40,25 +53,17 @@ function showUser(text) {
   saveUsers();
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = userInput.value;
-  showUser(currentValue);
-  userInput.value = "";
+function deleteUser(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  userList.removeChild(li);
+  const cleanUsers = users.filter((element) => {
+    return element.id !== Number(li.id);
+  });
+  users = cleanUsers;
+  saveUsers();
 }
 
-function loadName() {
-  const currentUsers = localStorage.getItem(USER_LS);
-  if (currentUsers !== null) {
-    const parsedUsers = JSON.parse(currentUsers);
-    parsedUsers.forEach((element) => {
-      showUser(element.text);
-    });
-  }
+function saveUsers() {
+  localStorage.setItem(USER_LS, JSON.stringify(users));
 }
-
-function init() {
-  loadName();
-  userForm.addEventListener("submit", handleSubmit);
-}
-init();
